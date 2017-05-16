59d2b20cc7195cd99d239887b60ca5691346bd61
sporangia <everhart@unl.edu>
2017-05-16 11:56:33 -0500

    New text

--- Part1-Introduction.R
+++ Part1-Introduction.R
@@ -14,7 +14,15 @@
 # -------------------------------
 # 
 # A script is a text document that contains instructions and commands
-# The # symbol is used to 
+# The # symbol is used to leave comments, which R will not try to interpret as a command
+#
+# The console (below) is for submitting commands to be interpreted in R. To run a 
+# command in the console, you can copy+paste it into the console and press enter.  
+# 
+# Run a single line of your script in the console by placing your cursor on the line
+# you want to submit and use your cursor to press the "Run" button at the top right.
+# You can also use the shortcut key strokes cmd+enter (mac) or ctrl+enter (PC) to run 
+# a single line at a time.
 #
 # Part 2: Creating objects
 # -------------------------
