f8ac022f031ff3c8185f3c8ea16c6fc998e6de42
sporangia <everhart@unl.edu>
2017-05-16 10:22:57 -0500

    Part 1 script beginning; created reference doc

--- Part1-Introduction.R
+++ Part1-Introduction.R
@@ -1,1 +1,25 @@
-# Introduction to R 
+# 
+# Let's get familiar with R
+# =========================
+# 
+# In this section we will investigate the following questions:
+#  [[  How does R understand data?  ]]
+#
+#  1. Introduction to RStudio
+#  2. Creating objects
+#  3. Sequences and vectors
+#  4. Matrices and evaluating a matrix
+#  5. 
+#  
+#
+#  1. Working directories
+#  2. Reading in data
+#  3. Finding help
+#  4. Basic data manipulation (renaming, subsetting, rotating)
+#  
+#  
+# Part 1: Working Directories
+# ---------------------------
+# 
+# A working directory is the folder on your computer where you are currently
+# working. You can find out your current working directory by typing `getwd()`
