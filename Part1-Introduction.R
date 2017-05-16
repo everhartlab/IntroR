88824ebb09214b8bf19bf7f23877ac93b4bff4c2
sporangia <everhart@unl.edu>
2017-05-16 11:26:35 -0500

    Updated part1 script

--- Part1-Introduction.R
+++ Part1-Introduction.R
@@ -1,1 +1,27 @@
-# Introduction to R 
+# 
+# Let's get familiar with R
+# =========================
+# 
+# In this section we will investigate the following questions:
+#  [[  How does R understand data?  ]]
+#
+#  1. How to use this R script
+#  2. Creating objects
+#  3. Sequences and vectors
+#  4. Matrices and evaluating a matrix#  
+#  
+# Part 1: How to use this R scrpt
+# -------------------------------
+# 
+# A script is a text document that contains instructions and commands
+# The # symbol is used to 
+#
+# Part 2: Creating objects
+# -------------------------
+# 
+# Part 3: Sequences and vectors
+# -----------------------------
+# 
+# Part 4: Matricies and evaluating a matrix
+# -----------------------------------------
+# 
