5f62de47d0ba89bc711ae218dde8016ec0103879
sporangia <everhart@unl.edu>
2017-05-17 11:58:07 -0500

    Revisions

--- Part1-Introduction.R
+++ Part1-Introduction.R
@@ -5,10 +5,10 @@
 # In this section we will investigate the following questions:
 #  [[  How does R understand data?  ]]
 #
-#  1. How to use this R script
+#  1. How to use this script and R
 #  2. Creating objects
-#  3. Sequences and vectors
-#  4. Matrices and evaluating a matrix # name?
+#  3. Sequences and vectors (1-D)
+#  4. Matrices and dataframes (2-D)
 #  
 # Part 1: How to use this R scrpt
 # -------------------------------
@@ -23,9 +23,8 @@
 # 
 # Copy and paste the following into the console and run it: 
 
-    print("the teacher's name is Sydney")  
- 
-#
+    print("the instructor's name is Sydney")  
+
 # Run a single line of your script in the console by placing your cursor on the
 # line you want to submit and use your cursor to press the "Run" button at the
 # top right. You can also use the shortcut key strokes cmd+enter (mac) or
@@ -34,16 +33,33 @@
 #The two other most important keyboard shortcuts that you'll want to use are the
 #*Tab* key to auto-complete your typing at the command line and ctrl+up arrow or
 #cmd+up arrow to access the most recently typed commands.
-
 #
 # You can also select only part of a line to have it run on the console.  Try running
 # the following command without copy+paste:   print("my name is _____") 
+#
+# R can also be used to perform calculations, such as the following:
+
+5+1/3
+
+# What rules does R apply for the order of operations?
+
+
+
+
 #
 #
 # Part 2: Creating objects
 # -------------------------
 #
-# It is easier to understand objects if we are working with a real data set. We have
+# It is easier to understand objects if we are working on a real problem.  The PI in your lab group wants you to 
+
+Let's
+# say we have a new fungicide for control of wheat stipe rust.  We want to
+# determine the amount of fungicide needed to control disease, first conducted
+# in a greenhouse experiment. the fungicide in a greenhouse experiment
+
+
+#We have
 # obtained data from an experiment that was designed to assess the best method for 
 # control of stripe rust disease. Fungicides can be used help reduce disease. 
 # 
